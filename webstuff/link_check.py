612f5598e19e22a2059be491fdf8bc5dad4fd0d6
Jeff Chheng <jrchheng@uw.edu>
2013-06-25 17:27:35 -0600

    Added command-line argument support

--- webstuff/link_check.py
+++ webstuff/link_check.py
@@ -1,55 +1,114 @@
 # Link Check goes through a URL, requests every link on the page, and prints
-# 	out the requested links and their respective status codes. It also saves
+#	out the requested links and their respective status codes. It also saves
 #	the statuses to requests.txt for further perusal.
 # By Jeff Chheng
 
 from BeautifulSoup import BeautifulSoup
 import urllib2
-import re
+import argparse
 
-# page to get info, replace with whatever (no trailing slash)
-url = "http://escience.washington.edu"
-
-# get/store info from page
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page.read())
-a = soup.findAll("a")
-links = []
-
-# format links
-for link in a:
-	href = link["href"]
-	if "http://" in href or "https://" in href:
-		links.append(href)
+#-------------------------------------------------------------------------------
+# parse_args
+#	Parse command-line arguments and calls other methods with appropriate
+#	parameters.
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--link",
+		help="Check the links on the specified web page",
+		nargs=1,
+		required=True)
+	
+	parser.add_argument("--log",
+		help="Log results to a file",
+		nargs=1,
+		default=False)
+	
+	args = parser.parse_args()
+	links = get_links(args.link[0])
+	if links:
+		if args.log:
+			check_links(links, args.log[0])
+		else:
+			check_links(links, args.log)
 	else:
-		if href.startswith("/"):
-			links.append("".join([url, href]))
-		else:
-			links.append("/".join([url, href]))
+		print "Your specified URL doesn't have any links to check."
 
-f = open("requests.txt", "w")
-
-# request links and write statuses to file
-for link in links:
-	requesting = "Requesting %s" % link
-	
-	print requesting
-	f.write("%s\n" % requesting)
-	
+#-------------------------------------------------------------------------------
+# get_links
+#	Get links from a specified url.
+#	Parameters:
+#		-- url: a url to retrieve links from
+#	Return a list of the links.
+def get_links(url):
+	# remove trailing slashes
+	while url.endswith("/"):
+		url = url[:-1]
+		
 	try:
-		request = urllib2.urlopen(link)
-		code = request.getcode()
+		# get/store info from page
+		page = urllib2.urlopen(url)
+	except urllib2.URLError, e:
+		print e
+		return None
+	
+	soup = BeautifulSoup(page.read())
+	a = soup.findAll("a")
+	links = []
 
-		if code == 200:
-			status = "200 OK\n"
+	# format links
+	for link in a:
+		href = link["href"]
+		if "http://" in href or "https://" in href:
+			links.append(href)
 		else:
-			status = "%s ERROR\n" % code
+			if href.startswith("/"):
+				links.append("".join([url, href]))
+			else:
+				links.append("/".join([url, href]))
+	
+	return links
+	
+
+#-------------------------------------------------------------------------------
+# check_links
+#	Check a list of links and print results of requests to stdout. Optionally,
+#	write results to a file.
+#	Parameters:
+#		-- links: a list of links
+#		-- log: a file to write the logs to
+def check_links(links, log):
+	if log:
+		file = open(log, "w")
+	else:
+		file = None
+
+	# request each link
+	for link in links:
+		requesting = "Requesting %s" % link
+		print requesting
 		
-		print status
-		f.write("%s\n" % status)
-		
-	except urllib2.HTTPError, e:
-		error = "%s\n" % e
-		print error
-		f.write("%s\n" % error)
-		
+		try:
+			request = urllib2.urlopen(link)
+			code = request.getcode()
+
+			# check HTTP status code
+			if code == 200:
+				status = "200 OK\n"
+			else:
+				status = "%s ERROR\n" % code
+			
+			print status
+			
+			if file:
+				file.write("%s\n" % requesting)
+				file.write("%s\n" % status)
+			
+		except urllib2.HTTPError, e:
+			error = "%s\n" % e
+			print error
+			if file:
+				file.write("%s\n" % requesting)
+				file.write("%s\n" % error)
+
+if __name__ == '__main__':
+	parse_args()
